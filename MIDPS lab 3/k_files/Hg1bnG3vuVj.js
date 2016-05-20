8c0959089f91928d60a414cb035bad56e45ab7ae
Ciulcov Dumitru <dmitrii122988@gmail.com>
2016-05-20 11:07:36 +0300

    removed some files

--- MIDPS lab 3/k_files/Hg1bnG3vuVj.js
+++
@@ -1,5 +0,0 @@
-/*!CK:2603673098!*//*1459280633,*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["cEK9F"]); }
-
-__d('AccessibilityWebVirtualCursorClickLogger',['AccessibilityWebAssistiveTechTypedLogger','VirtualCursorStatus'],function a(b,c,d,e,f,g){if(c.__markCompiled)c.__markCompiled();var h={init:function(i){c('VirtualCursorStatus').add(i,this._log);},_log:function(i,j){if(i)new (c('AccessibilityWebAssistiveTechTypedLogger'))().setIndicatedBrowsers(j).log();}};f.exports=h;},null);
